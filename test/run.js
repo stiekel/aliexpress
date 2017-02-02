4aafd4d5067984dd85efdfed8819c19cc2a5b1a0
stiekel <histkc@gmail.com>
2017-02-02 20:39:08 +0800

    reorganize test

---
+++ test/run.js
@@ -0,0 +1,8 @@
+describe('AliExpress', () => {
+  describe('test.js', () => {
+    require('./test.js')
+  })
+  describe('search.js', () => {
+    require('./search.js')
+  })
+})
--- test/test.js
+++ test/test.js
@@ -3,34 +3,32 @@ var Spider = require('../lib/Spider');
 var assert = require('assert');
 var should = require('should');
 
-describe('AliExpress', function(){
-  var good;
-  describe('Spider', function(){
-    var url = 'http://qq.com';
-    it(['request', url, 'show return a web page'].join(' '), function(){
-      var spider = new Spider('http://qq.com/');
-      return spider.getHTML().then(function(d){
-        d.should.be.an.String();
-      });
+var good;
+describe('Spider', function(){
+  var url = 'https://www.so.com/?src=so.com';
+  it(['request', url, 'show return a web page'].join(' '), function(){
+    var spider = new Spider(url);
+    return spider.getHTML().then(function(d){
+      d.l.be.an.String();
     });
   });
-  describe('Best Selling', function() {
-    it('should return a list of itesm', function(){
-      return AliExpress.BestSelling.get().then(function(data){
-        data.should.be.an.instanceOf(Array);
-        good = data[0] || null;
-        good.should.be.an.instanceOf(Object)
-        good.url.should.be.an.String();
-      });
+});
+describe('Best Selling', function() {
+  it('should return a list of items', function(){
+    return AliExpress.BestSelling.get().then(function(data){
+      data.should.be.an.instanceOf(Array);
+      good = data[0] || null;
+      good.should.be.an.instanceOf(Object)
+      good.url.should.be.an.String();
     });
   });
+});
 
-  describe('Detail', function () {
-    it('showuld return Good Detail', function(){
-      return AliExpress.Detail(good.url).then(function(d){
-        d.should.be.an.instanceOf(Object);
-        d.productId.should.be.an.String();
-      });
+describe('Detail', function () {
+  it('showuld return Good Detail', function(){
+    return AliExpress.Detail(good.url).then(function(d){
+      d.should.be.an.instanceOf(Object);
+      d.productId.should.be.an.String();
     });
   });
 });
